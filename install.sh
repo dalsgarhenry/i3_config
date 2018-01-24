c24fbc4310fb303e61681d372c9754187c5ea6f8
Henry <dalsgar.henry@gmail.com>
2018-01-24 19:35:53 +0100

    init

---
+++ install.sh
@@ -0,0 +1,21 @@
+# This is a installation file for setting up i3wm in a sain mannor when using ubuntu..
+
+# Make it possible to run nautiluse
+gsettings set org.gnome.desktop.background show-desktop-icons false
+
+# Add i3 config file
+
+# Nice utils
+sudo apt-get install -y htop \
+    tmux \
+    i3 \
+    i3blocks \
+    vim \
+    redshift \
+    lxappearance \
+    xfwm4-themes \
+    gksu
+    
+
+# extra
+# geany
